cf4aa93e458a2c9c9ec0b3677a49a30cd82d6807
BeroKiTeer <1377433202@qq.com>
2023-11-09 17:27:14 +0800

    2023.11.9 17:27

--- include/LittleMap.h
+++ include/LittleMap.h
@@ -10,7 +10,7 @@ public:
     void SetPlayerPosition(int x, int y);   //设置玩家位置
     void DrawTargetPosition(int x, int y);  //绘制目标位置
     void ClearMap();        //清空小地图
-    
+
 private:
     GLuint mapTexture;    //贴图
     int playerX, playerY; // 玩家位置
