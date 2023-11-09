f3d839e9306995300348526ba00e9dc2d192ec28
BeroKiTeer <1377433202@qq.com>
2023-11-09 18:09:47 +0800

    2023.11.9 18:09

--- include/Person.h
+++ include/Person.h
@@ -11,8 +11,7 @@ public:
     void move(double delx,double delz);//移动
     void makeMatch(double topx, double topy, double bottomx, double bottomy,
     double matchWidth, double topd, double bottomd, std::vector <Point> &res);
-    //用于创建一个特定部分（比如腿、手臂等）的3D模型，该部分由两个圆柱体组成
-    //：一个顶部圆柱体和一个底部圆柱体。
+    //其实就是画圆柱，用于创建一个特定部分（比如腿、手臂等）的3D模型
     /*
             输入参数：
             topx, topy：顶部圆柱体的中心坐标。
@@ -23,9 +22,14 @@ public:
             getCircle 函数被调用两次来获取顶部和底部圆柱体的顶点信息，这些信息存储在 top 和 bottom 向量中。
      * 
      */
+    //：一个顶部圆柱体和一个底部圆柱体。
+    
     void makeBody();//创建角色身体
+
     void initInd(const std::vector <Point> &leftLeg, const std::vector <Point> &rightLeg,
 	const std::vector <Point> &leftArm, const std::vector <Point> &rightArm);
+
+    //移动时的动作
     void rotateIndLeftLeg(std::vector <Point> &v, double angle);
     void rotateIndRightLeg(std::vector <Point> &v, double angle);
     void rotateIndLeftArm(std::vector <Point> &v, double angle);
@@ -34,8 +38,15 @@ public:
     void swingIndLeg(double angle, double depth);
     void bodyTranslateY(double delta);
 
+protected:
+    bool visable = true;
+    bool firstPerson;
+
+    bool moveLeft = false, moveRight = false, moveForward = false, moveBack = false;//人物移动
+    bool jump = false,turnLeft = false,turnRight = false;
+
 private:
+    //人物属性
     double bodyWidth = 3, bodyHeight = 100, armStepWidth = 20,
     legStepWidth = 15, armLongerThanLeg = 10;
-    bool visable = true;
 };
