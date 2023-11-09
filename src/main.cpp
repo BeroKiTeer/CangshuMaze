f5677c30a1fe50e4bb94f7996271f73020877478
BeroKiTeer <1377433202@qq.com>
2023-11-09 10:58:40 +0800

    2023.11.9 10:58

--- src/main.cpp
+++ src/main.cpp
@@ -74,34 +74,5 @@ int main (int argc, char** argv)
     glutTimerFunc(ACTORJUMPSPEED, actJumpLoop, ACTORJUMPID);        //于处理角色或对象的跳跃动画
     glutMainLoop();
 
-    //渲染
-    glEnable(GL_TEXTURE_2D);
-    glutDisplayFunc([](){
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDisable(GL_DEPTH_TEST);       //关闭深度测试，即禁止OpenGL对物体的深度进行检测。这是在绘制一些不需要深度排序的对象时使用的。
-        glLoadIdentity();   //重置当前模型视图矩阵为单位矩阵
-        /*
-
-        glRotated(obvRotateY, 1, 0, 0);
-        glRotated(obvRotateX, 0, 1, 0);
-         */
-        //分别对X轴和Y轴进行旋转。用于调整视角或相机的方向。
-        //addSky();渲染天空盒
-
-        glEnable(GL_DEPTH_TEST);    //重新启用深度测试，以确保渲染的物体根据它们的深度位置正确排序。
-        glLoadIdentity();
-        glLineWidth(3);
-        //updateObverse();
-        //addGround();更新OpenGL的观察视角（视图矩阵）
-        // addWall();
-        // addActor();
-
-        // drawLine(-300, 0, 0, 300, 0, 0, WHITE);
-        // drawLine(0, -300, 0, 0, 300, 0, WHITE);
-        // drawLine(0, 0, -300, 0, 0, 300, WHITE);
-        glutSwapBuffers();
-    }); 
-    glutReshapeFunc(reshape);
-
     return 0;
 }
