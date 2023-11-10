b21df404def2bdf642a381b42b7c9f1ca291efda
Jordon5 <bvj54286117@163.com>
2023-11-10 16:46:30 +0800

    16:46

--- include/camera.h
+++ include/camera.h
@@ -10,13 +10,13 @@ public:
     Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat targetX, GLfloat targetY, GLfloat targetZ);
     ~Camera() = default;
 
-    void setPosition(GLfloat posX, GLfloat posY, GLfloat posZ);
-    void setTarget(GLfloat targetX, GLfloat targetY, GLfloat targetZ);
-    void setUpVector(GLfloat upX, GLfloat upY, GLfloat upZ);
+    void setPosition(GLfloat posX, GLfloat posY, GLfloat posZ);//设置位置
+    void setTarget(GLfloat targetX, GLfloat targetY, GLfloat targetZ);//设置目标
+    void setUpVector(GLfloat upX, GLfloat upY, GLfloat upZ);//设置向量
     void updateViewMatrix();        //根据相机位置更新渲染视图
-    void setProjectionMatrix(GLfloat fov, GLfloat aspectRatio, GLfloat nearPlane, GLfloat farPlane);
-    void rotate(GLfloat deltaYaw, GLfloat deltaPitch);
-    void move(GLfloat deltaX, GLfloat deltaY, GLfloat deltaZ);
+    void setProjectionMatrix(GLfloat fov, GLfloat aspectRatio, GLfloat nearPlane, GLfloat farPlane);//设置项目矩阵
+    void rotate(GLfloat deltaYaw, GLfloat deltaPitch);//旋转
+    void move(GLfloat deltaX, GLfloat deltaY, GLfloat deltaZ);//前后左右
     GLfloat* GetViewMatrix();
 
     void firstPerson();//第一人称视角
