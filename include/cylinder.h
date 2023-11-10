0be6f0a9bfc70dc5510dcc9747d311b9a7e40159
BeroKiTeer <1377433202@qq.com>
2023-11-10 12:20:06 +0800

    2023.11.10 12:19

--- include/cylinder.h
+++ include/cylinder.h
@@ -7,6 +7,8 @@
 class Cylinder {
 public:
     Cylinder(GLfloat radius,GLfloat height, GLint segments);
+    Cylinder(Point p,GLfloat radius,GLfloat height, GLint segments);
+    Cylinder(GLfloat x, GLfloat y, GLfloat z,GLfloat radius,GLfloat height, GLint segments);
 
     void GenerateCylinderVertices();
 
@@ -14,8 +16,11 @@ public:
 private:
     GLfloat radius;
     GLfloat height;
+    GLfloat diameter;
     GLint segments;
+    Point p;        //圆柱中心点
+    GLfloat x,y,z;  //圆柱中心点
 
-    std::vector<GLfloat> vertices; // 顶点坐标
-    std::vector<GLfloat> normals;  // 法线
+    std::vector<Point> topCircle; // 顶部圆形的顶点
+    std::vector<Point> bottomCircle;  // 底部圆形的顶点
 };
--- src/cylinder.cpp
+++ src/cylinder.cpp
@@ -1,5 +1,56 @@
 #include "cylinder.h"
 
 Cylinder::Cylinder(GLfloat radius, GLfloat height, GLint segments)
-:radius(radius),height(height),segments(segments){}
+:radius(radius),height(height),segments(segments)
+{
+    diameter = 2*radius;
+    getCircle(x, y, radius, z, bottomCircle);
+    getCircle(x, y, radius, z + height, topCircle);
+}
 
+Cylinder::Cylinder(Point p, GLfloat radius, GLfloat height, GLint segments)
+:p(p),radius(radius),height(height),segments(segments)
+{
+    diameter = 2*radius;
+    getCircle(p.x, p.y, radius, p.z, bottomCircle);
+    getCircle(p.x, p.y, radius, p.z + height, topCircle);
+}
+
+Cylinder::Cylinder(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLfloat height, GLint segments)
+:x(x),y(y),z(z),radius(radius),height(height),segments(segments)
+{
+    diameter = 2*radius;
+    getCircle(x, y, radius, z, bottomCircle);
+    getCircle(x, y, radius, z + height, topCircle);
+}
+
+void Cylinder::render()
+{
+     glBegin(GL_TRIANGLE_FAN);
+    for (const auto& point : bottomCircle) {
+        glVertex3d(point.x, point.y, point.z);
+    }
+    glEnd();
+
+    // 渲染顶部圆形
+    glBegin(GL_TRIANGLE_FAN);
+    for (const auto& point : topCircle) {
+        glVertex3d(point.x, point.y, point.z);
+    }
+    glEnd();
+
+    // 渲染侧面
+    glBegin(GL_TRIANGLE_STRIP);
+    for (int i = 0; i <= segments; ++i) {
+        double theta = 2.0 * M_PI * i / segments;
+        double dx = radius * cos(theta);
+        double dy = radius * sin(theta);
+
+        // 底部点
+        glVertex3d(x + dx, y + dy, z);
+
+        // 顶部点
+        glVertex3d(x + dx, y + dy, z + height);
+    }
+    glEnd();
+}
